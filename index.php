fd668c0ed967371071e076b277e7e32e85ad107a
Derek Moeller <derek@goremedy.com>
2014-04-17 11:51:17 -0700

    Added webpage framework

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<?php
-	$num = 0;
-	for (
-		$num<256; 
-		$num++;
-		)
+<html>
+<head>
+</head>
+<body>
+	<?php
+		$num = 0;
+			for ($num<256; $num++;)
 		{echo $num; 
 		echo <br>;
 		}
-?>
+	?>
+</body>
+</html>
