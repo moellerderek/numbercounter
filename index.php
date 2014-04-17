d0f50ccc3ebd75f582c9b65c5cba9883737a675b
Derek Moeller <derek@goremedy.com>
2014-04-17 12:29:37 -0700

    Columed

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 <title>Number Counter</title>
 </head>
 <body>
-<h2>Counting to <?php $num ?>:
+<h2>Counting to <?php $num ?>:</h2>
+	<br>
+	<table>
 	<?php
 			for ($num=0; $num<=255; $num++)
 			{
-			echo $num."<br>";
+			echo <td>
+			echo $num."<br>"
+			echo </td>;
 			}
 	?>
 </body>
