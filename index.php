300bd330db9ec11416f9268ed4952a9a9c917678
Derek Moeller <derek@goremedy.com>
2014-04-17 14:45:29 -0700

    fromat

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo "<h2>Counting towards". $numend.":"."</h2>";
 
 print "<br>.<br>";
 
-echo "<h2>Counting backwards from"..$numend.":"."</h2>";
+echo "<h2>Counting backwards from". $numend.":"."</h2>";
 print "<br>";
 
 			for ($num=$numend; $num>=$numstart; $num--)
