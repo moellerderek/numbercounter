0b4d35ead8f8b6e6aeb8fa1f5ec148c6fdd34e58
Derek Moeller <derek@goremedy.com>
2014-04-24 13:51:37 -0700

    Fixed include.

--- index.php
+++ index.php
@@ -60,29 +60,12 @@
       <div class="row">
         <div class="col-md-6">
         <?php
-        $num = $_GET['num'];
-        echo "<h2>"."Counting Towards".$num."</h2>"."<p>";
-  
-		if (!isset($num)) die("Give me a number, boo");
-		if ($num < 0) die ("Yo, those digits are not valid, bro...");
 
-			for ($i = 0; $i <= $num; $i++) 
-			{
-			echo "$i"."<br>";	
-			}
-			
-echo "</div>"."</div>"."</div>"."<br>";
+        include ('counter.php');
 
-echo '<div class="col-md-6">''<div class="from">'.'<h2>Counting backwards from '.$num.':'.'</h2>';
-echo '<br>';
-
-			for ($i = $num; $i >= 0; $i--) 
-			{
-			echo "$i"."<br>";	
-			}
-echo '</div>.'</div>';
-
-?>
-<br>
+        ?>
+        <br>
+      </div>
+    </div>
 </body>
 </html>
