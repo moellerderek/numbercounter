e0cb18afd272752024d77cb40cd4a71a8ca2ff83
Derek Moeller <derek@goremedy.com>
2014-04-24 10:51:53 -0700

    Moving some things around added bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <html>
 <head>
 <title>Number Counter</title>
-<link href="index.css" rel="stylesheet">
+<link href="/css/index.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Alike' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css'>
-</head>
-<body>
+<!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
@@ -40,7 +54,7 @@
         <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
-
+<br>
 <?php
 
   $num = $_GET['num'];
